83bae23be262e0492287510e580e606028de23c2
bakayro <bakayrooleg@gmail.com>
2022-01-15 15:12:52 +0100

    new commit

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,51 +1,51 @@
 new WOW().init();
- 
- $(".scroll-up").click(function() {
+
+$(".scroll-up").click(function () {
 	$('html, body').animate({
 		scrollTop: 0
 	}, 800);
 	return false;
 });
-			
-$(".anchor-link").click(function(e){
+
+$(".anchor-link").click(function (e) {
 	var data_scroll = $($(this).data("scroll"));
-	$("html, body").animate({scrollTop: data_scroll.offset().top},"slow","swing",function(){});
+	$("html, body").animate({ scrollTop: data_scroll.offset().top }, "slow", "swing", function () { });
 	return false;
 });
 
-$("a[href='#top']").click(function() {
+$("a[href='#top']").click(function () {
 	$("html, body").animate({ scrollTop: 0 }, 100);
 	return false;
 });
 
-	/* COUNT-UP */
-	$('.counter-1').counterUp({
-		delay: 10,
-		time: 1750
-	});
-	
-	$('.counter-2').counterUp({
-		delay: 5,
-		time: 2000
-	});
-	
-	$('.counter-3').counterUp({
-		delay: 10,
-		time: 1750
-	});
-	$('.counter-4').counterUp({
-		delay: 10,
-		time: 1750
-	});
+/* COUNT-UP */
+$('.counter-1').counterUp({
+	delay: 10,
+	time: 1750
+});
 
-$(document).ready(function(){
+$('.counter-2').counterUp({
+	delay: 5,
+	time: 2000
+});
+
+$('.counter-3').counterUp({
+	delay: 10,
+	time: 1750
+});
+$('.counter-4').counterUp({
+	delay: 10,
+	time: 1750
+});
+
+$(document).ready(function () {
 
 	/* OWL-SLIDER */
-  $('.owl-carousel').owlCarousel({
-    items: 1,
-    loop: true,
-	smartSpeed: 1500,
-    margin: 10,
-    nav: false,
+	$('.owl-carousel').owlCarousel({
+		items: 1,
+		loop: true,
+		smartSpeed: 1500,
+		margin: 10,
+		nav: false,
 	})
 });
